68dcc8dc2931bb30f588a36165addaf590b6fd01
Ishmum <ishmum20000@gmail.com>
2024-07-22 17:01:09 +0800

    Add logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+      <title>Logo</title>
+      <g>
+        <g id="T" transform="translate(40, 35)">
+          <path
+            fill="currentColor"
+            d="M 0 0 L 20 0 L 20 5 L 12.5 5 L 12.5 25 L 7.5 25 L 7.5 5 L 0 5 Z"
+          />
+        </g>
+        <path
+          stroke="currentColor"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M 50, 5
+                  L 11, 27
+                  L 11, 72
+                  L 50, 95
+                  L 89, 73
+                  L 89, 28 z"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default Logo;
