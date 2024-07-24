b731c8da688990cda76f4e2e91204548fd4e5a88
Ishmum <ishmum20000@gmail.com>
2024-07-24 12:25:04 +0800

    Add navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -3,6 +3,16 @@ import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
+  const sectionLinks = [
+    { name: "About", link: "/#about" },
+    { name: "Experience", link: "/#experience" },
+    { name: "Work", link: "/#work" },
+    {
+      name: "Contact",
+      link: "/#contact",
+    },
+  ];
+
   return (
     <nav>
       <div className="wrapper">
@@ -10,6 +20,17 @@ const Navbar = () => {
           <Link href="/">
             <Logo />
           </Link>
+        </div>
+        <div className="nav-items">
+          <ul className="nav-items-list">
+            {
+              sectionLinks.map((item, index) => (
+                <li key={index} className='nav-items-list-item'>
+                  <Link href={item.link} className='nav-items-list-item-link'>{item.name}</Link>
+                </li>
+              ))
+            }
+          </ul>
         </div>
       </div>
     </nav>
