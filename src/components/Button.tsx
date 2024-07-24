01305a14ac668cfe3d0366cc8cfebcac106140bd
Ishmum <ishmum20000@gmail.com>
2024-07-24 13:06:49 +0800

    Format code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,12 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-const Button = ({text, link}: {text: string, link: string}) => {
-  return (
-    <Link href={link} className='btn'>{text}</Link>
-  )
-}
+const Button = ({ text, link }: { text: string; link: string }) => {
+	return (
+		<Link href={link} className="btn">
+			{text}
+		</Link>
+	);
+};
 
-export default Button
+export default Button;
