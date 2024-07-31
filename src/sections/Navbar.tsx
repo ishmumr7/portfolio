2012ea92bdc2b5a9ae0684370e06a66986de9538
Ishmum <ishmum20000@gmail.com>
2024-07-31 20:45:31 +0800

    Make navbar responsive

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,54 +1,98 @@
-"use client"
+"use client";
 
-import Button from '@/components/Button';
-import Logo from '@/components/Logo'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+import Button from "@/components/Button";
+import Logo from "@/components/Logo";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { CgClose } from "react-icons/cg";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-  const sectionLinks = [
-    { name: "About", link: "/#about" },
-    { name: "Experience", link: "/#experience" },
-    { name: "Work", link: "/#work" },
-    {
-      name: "Contact",
-      link: "/#contact",
-    },
-  ];
+	const sectionLinks = [
+		{ name: "About", link: "/#about" },
+		{ name: "Experience", link: "/#experience" },
+		{ name: "Work", link: "/#work" },
+		{
+			name: "Contact",
+			link: "/#contact",
+		},
+	];
 
-  const [visible, setVisible] = useState(false);
+	const [visible, setVisible] = useState(false);
+	const [responsiveVisible, setResponsiveVisible] = useState(false);
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      window.scrollY > 100 ? setVisible(true) : setVisible(false);
-    })
-  })
+	useEffect(() => {
+		window.addEventListener("scroll", () => {
+			window.scrollY > 100 ? setVisible(true) : setVisible(false);
+		});
+	});
 
-  return (
-    <nav>
-      <div className={`wrapper ${visible && "blur-nav"}`}>
-        <div className="brand">
-          <Link href="/">
-            <Logo />
-          </Link>
-        </div>
-        <div className="nav-items">
-          <ul className="nav-items-list">
-            {
-              sectionLinks.map((item, index) => (
-                <li key={index} className='nav-items-list-item'>
-                  <Link href={item.link} className='nav-items-list-item-link'>{item.name}</Link>
-                </li>
-              ))
-            }
-          </ul>
-          <div className="nav-items-button">
-            <Button text="Resume" link="http://localhost:3000/Resume.pdf" />
-          </div>
-        </div>
-      </div>
-    </nav>
-  )
-}
+	useEffect(() => {
+		const links = document.querySelectorAll(".nav-items-list-item-link");
+		links.forEach((link) => {
+			link.addEventListener("click", () => setResponsiveVisible(false));
+		});
+		const nav = document.querySelector(".nav-items");
+		nav?.addEventListener("click", (e) => {
+			e.stopPropagation();
+		});
+		const html = document.querySelector("html");
+		html?.addEventListener("click", (e) => {
+			setResponsiveVisible(false);
+		});
+	}, []);
 
-export default Navbar
+	useEffect(() => {
+		const main = document.querySelector("main");
+		if (responsiveVisible) {
+			main?.classList.add("blur");
+		} else {
+			main?.classList.remove("blur");
+		}
+	}, [responsiveVisible]);
+
+	return (
+		<nav>
+			<div className={`wrapper ${visible && "blur-nav"}`}>
+				<div className="brand">
+					<Link href="/">
+						<Logo />
+					</Link>
+				</div>
+				<div className="nav-responsive-toggle">
+					{responsiveVisible ? (
+						<CgClose
+							onClick={(e) => {
+								e.stopPropagation();
+								setResponsiveVisible(false);
+							}}
+						/>
+					) : (
+						<GiHamburgerMenu
+							onClick={(e) => {
+								e.stopPropagation();
+								setResponsiveVisible(true);
+							}}
+						/>
+					)}
+				</div>
+				<div className={`${responsiveVisible && "nav-responsive"} nav-items`}>
+					<ul className="nav-items-list">
+						{sectionLinks.map((item, index) => (
+							<li key={index} className="nav-items-list-item">
+								<Link href={item.link} className="nav-items-list-item-link">
+									{item.name}
+								</Link>
+							</li>
+						))}
+					</ul>
+					<div className="nav-items-button">
+						<Button text="Resume" link="http://localhost:3000/Resume.pdf" />
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
