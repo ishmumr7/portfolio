f19d8cef6bd07344b48b4822a3447fe31352a01f
Ishmum <ishmum20000@gmail.com>
2024-08-03 21:47:57 +0800

    Add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,14 @@ import About from "@/sections/About";
 import Experience from "@/sections/Experience";
 import Projects from "@/sections/Projects";
 import Contact from "@/sections/Contact";
+import Head from "next/head";
 
 export default function Home() {
 	return (
 		<div>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<Navbar />
 			<Socials />
 			<Email />
