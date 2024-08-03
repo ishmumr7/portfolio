c1bbad7824c580a66b333bcb75d152befe944a39
Ishmum <ishmum20000@gmail.com>
2024-08-03 22:16:16 +0800

    Add loader and its animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Email from "@/components/Email";
 import Socials from "@/components/Socials";
 import Navbar from "@/sections/Navbar";
@@ -7,23 +8,38 @@ import Experience from "@/sections/Experience";
 import Projects from "@/sections/Projects";
 import Contact from "@/sections/Contact";
 import Head from "next/head";
+import { useState } from "react";
+import Loader from "@/components/Loader";
 
 export default function Home() {
+	const [isLoading, setIsLoading] = useState(true);
+	const [showContent, setShowContent] = useState(false);
+
+	const handleLoaderLoaded = () => {
+		setIsLoading(false);
+		setTimeout(() => setShowContent(true), 450);
+	};
+
 	return (
 		<div>
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Navbar />
-			<Socials />
-			<Email />
-			<main>
-				<Hero />
-        <About />
-				<Experience />
-				<Projects />
-				<Contact />
-			</main>
+			{showContent && (
+				<>
+					<Navbar />
+					<Socials />
+					<Email />
+					<main>
+						<Hero />
+						<About />
+						<Experience />
+						<Projects />
+						<Contact />
+					</main>
+				</>
+			)}
+			<Loader isLoading={isLoading} setIsLoading={handleLoaderLoaded} />
 		</div>
 	);
 }
